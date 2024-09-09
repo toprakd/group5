c63d09db0594e4404a92871a4e4c4370f887c81c
toprakd <h.toprak164@gmail.com>
2024-09-09 14:35:01 +0200

    delete duplicates and pivot wider

--- SCRIPT/script_day_5.R
+++ SCRIPT/script_day_5.R
@@ -4,13 +4,21 @@ library(tidyverse)
 library(here)
 
 
-# Exploring the data
+#read data
 exam_nontidy <- read_delim(here("DATA", "exam_nontidy.txt"))
 
-
+# Exploring the data
 head(exam_nontidy)
 tail(exam_nontidy,15)
 summary(exam_nontidy)
 str(exam_nontidy)
 glimpse(exam_nontidy)
 skimr::skim(exam_nontidy)
+
+
+
+#remove duplicates and pivot wider (for the measured variable and the .value coloumn)
+test <- exam_nontidy %>%
+  distinct() %>% 
+  pivot_wider(names_from = `measured variable`, 
+              values_from = .value)
