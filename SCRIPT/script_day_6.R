d52aa1367901efbeb2529eaee58c2efacdf05275
Cathrine Loddervik <cathrine@Cathrines-MacBook-Pro.local>
2024-09-10 13:06:29 +0200

    Arrange ID column of your dataset in order of increasing number or alphabetically

--- SCRIPT/script_day_6.R
+++ SCRIPT/script_day_6.R
@@ -1,3 +1,9 @@
 library(tidyverse)
 library(here)
 tidy_data <- read.delim(here("DATA/exam_data_tidy2024-09-09.txt"))
+
+
+
+#  Arrange ID column of your dataset in order of increasing number or alphabetically
+tidy_data <- tidy_data %>%
+  arrange(ID)
