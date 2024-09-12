06d6ef652c9ec0f10a7df62e13421a382756230a
Cathrine Loddervik <cathrine@Cathrines-MacBook-Pro.local>
2024-09-12 11:47:01 +0200

    Comment on the statistical tests

--- SCRIPT/day8.R
+++ SCRIPT/day8.R
@@ -4,30 +4,26 @@ library(here)
 #install.packages("ggcorrplot")
 
 
-#read data ----
+# Read data & run Kruskal test ----
 exam_tidy<- read_delim(here("DATA", "exam_data_tidy_modified2024-09-11.txt"))
-
-
-exam_tidy %>% 
-  wilcox.test(diabetes_5_year~pedigree, data = .) %>%
-  broom::tidy()
 
 exam_tidy %>% 
   kruskal.test(diabetes_5_year~pedigree, data = .) %>%
   broom::tidy()
-
+# The effect of pedigree on diabetes outcome was not statistically significant
 
 exam_tidy %>% 
   kruskal.test(diabetes_5_year~bmi, data = .) %>%
   broom::tidy()
-
+# The effect of BMI on diabetes was statistically significant 
 
 exam_tidy %>% 
   kruskal.test(diabetes_5_year~glucose_mg_dl, data = .) %>%
   broom::tidy()
-
+# The effect of glucose_mg_dl on diabetes was statistically significant 
 
 
 exam_tidy %>% 
   kruskal.test(diabetes_5_year~hospital, data = .) %>%
   broom::tidy()
+# The effect of hospital on diabetes was not statistically significant 
